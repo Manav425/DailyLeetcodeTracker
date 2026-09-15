ed859fe9ef932bb53cfc4fc61f94961533f5c6ad
Manav425 <manavbatra425@gmail.com>
2026-09-16 01:04:26 +0530

    Solve LeetCode Subarray Sum Equals K

---
+++ 560-subarray-sum-equals-k/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int cnt=0;
+        for(int i=0; i<nums.length; i++){
+            int s=0;
+            for(int j=i; j<nums.length; j++){
+                s+=nums[j];
+                if(s==k){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
